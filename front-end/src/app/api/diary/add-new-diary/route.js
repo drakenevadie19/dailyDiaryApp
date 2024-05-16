f04a30b4ad4e164b0c6ed4b07b3f7f43588b5451
drakenevadie19 <drakennievadje19@gmail.com>
2024-05-16 18:09:52 -0500

    New Change:  Successfully create POST API to push data into the database. Next is to connect FE and BE

---
+++ front-end/src/app/api/diary/add-new-diary/route.js
@@ -0,0 +1,34 @@
+// If having multiple routes, add more folders in diary folder
+
+import connectToDB from "@/database";
+import Diary from "@/models/diary";
+import { NextResponse } from "next/server";
+
+// Create a post method
+export async function POST(req) {
+    try {
+        await connectToDB();
+        // Getting title and description from inputting => {title, description} --> extractData
+        const extractData = await req.json();
+        // Create new Diary model
+        const newlyCreatedDiaryData = await Diary.create(extractData);
+        // If the model is created successfully, the return success == true, else return false;
+        if (newlyCreatedDiaryData) {
+            return NextResponse.json({
+                success: true, 
+                message: 'Diary added successfully'
+            })
+        } else {
+            return NextResponse.json({
+                success: false,
+                message: "Failed to add new diary to database! Please try again"
+            })
+        }
+    } catch (e) {
+        console.log(e);
+        return NextResponse.json({
+            success: false,
+            message: 'Something went wrong'
+        })
+    }
+}
