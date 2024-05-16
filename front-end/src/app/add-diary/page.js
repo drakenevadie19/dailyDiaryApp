5a598e6f5fc110d69c1d9b9bfde261df4145dde1
drakenevadie19 <drakennievadje19@gmail.com>
2024-05-16 12:40:29 -0500

    New Change: Adding components for AddDiary component

--- front-end/src/app/add-diary/page.js
+++ front-end/src/app/add-diary/page.js
@@ -5,6 +5,19 @@ export default function AddDiary() {
         <>
             <div>
                 <h1 className="font-bold text-lg">Add a new Diary for healing your day</h1>
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-3">
+                        <label><b><i>Enter Diary title</i></b></label>
+                        <input name="title" placeholder="Enter your diary title" />
+                    </div>
+                    <div className="flex flex-col gap-3">
+                        <label><b><i>Enter Diary title</i></b></label>
+                        <textarea name="title" placeholder="Enter your thought here" />
+                    </div>
+                    <div>
+                        <button>Add</button>
+                    </div>
+                </div>
             </div>
         </>
     )
