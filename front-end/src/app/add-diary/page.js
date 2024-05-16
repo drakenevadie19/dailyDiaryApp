110f35131b849c50d7301755406479d283e084c0
drakenevadie19 <drakennievadje19@gmail.com>
2024-05-16 13:11:38 -0500

    New Change: Adding functions  for AddDiary page

--- front-end/src/app/add-diary/page.js
+++ front-end/src/app/add-diary/page.js
@@ -1,21 +1,31 @@
-'use client' 
+'use client'
+
+import { useState } from "react";
+
+const initialFormData = {
+    title: '', 
+    description: ''
+}
 
 export default function AddDiary() {
+
+    const [diaryFormData, setDiaryFormData] = useState(initialFormData);
+
     return (
         <>
-            <div>
-                <h1 className="font-bold text-lg">Add a new Diary for healing your day</h1>
+            <div className="flex min-h-screen flex-col p-8">
+                <h1 className="font-bold text-lg mb-3">Add a new Diary for healing your day</h1>
                 <div className="flex flex-col gap-4">
                     <div className="flex flex-col gap-3">
                         <label><b><i>Enter Diary title</i></b></label>
-                        <input name="title" placeholder="Enter your diary title" />
+                        <input name="title" placeholder="Enter your diary title" className="border border-red-500 p-3 outline-none" value={diaryFormData['title']} onChange={(e) => setDiaryFormData({...diaryFormData, title: e.target.value})} />
                     </div>
                     <div className="flex flex-col gap-3">
                         <label><b><i>Enter Diary title</i></b></label>
-                        <textarea name="title" placeholder="Enter your thought here" />
+                        <textarea name="description" placeholder="Enter your thought here" rows={6} className="border border-red-500 p-3 outline-none" value={diaryFormData['description']} onChange={(e) => setDiaryFormData({...diaryFormData, description: e.target.value})} />
                     </div>
                     <div>
-                        <button>Add</button>
+                        <button className="border border-red-500 p-4 bg-black text-white" onClick={() => alert(`${diaryFormData['title']} ${diaryFormData['description']}`)}>Add</button>
                     </div>
                 </div>
             </div>
