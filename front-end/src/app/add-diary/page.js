e9aa1cfc29872ce87b8e626a2cdb04ba3483e719
drakenevadie19 <drakennievadje19@gmail.com>
2024-05-16 12:22:46 -0500

    New Change: Create basic ppages such as add diary page and diray list page

---
+++ front-end/src/app/add-diary/page.js
@@ -0,0 +1,11 @@
+'use client' 
+
+export default function AddDiary() {
+    return (
+        <>
+            <div>
+                <h1 className="font-bold text-lg">Add a new Diary for healing your day</h1>
+            </div>
+        </>
+    )
+}
---
+++ front-end/src/app/diary-list/page.js
@@ -0,0 +1,11 @@
+'use client' 
+
+export default function DiaryList() {
+    return (
+        <>
+            <div>
+                <h1 className="font-bold text-lg">Your journey of diary</h1>
+            </div>
+        </>
+    )
+}
