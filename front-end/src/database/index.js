b63f11e3b1a53b8f29ceb1a1a566f59b0a6ec4f4
drakenevadie19 <drakennievadje19@gmail.com>
2024-05-17 15:33:29 -0500

    New Change: Change CSS and background image of page

--- front-end/src/database/index.js
+++ front-end/src/database/index.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
 
 const connectToDB = async () => {
-    mongoose.connect(
-        'mongodb+srv://thanhnguyen14gers:Yr3qfOo8kWp0ehre@cluster0.clahwyv.mongodb.net/'
-    ).then(() => console.log("Database connected successfully"))
+    mongoose.connect('mongodb+srv://thanhnguyen14gers:wONnFfiTHq80zI66@cluster0.flb3538.mongodb.net/')
+    .then(() => console.log("Database connected successfully"))
     .catch((e) => console.log(e));
 }
 
