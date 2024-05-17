86e35829a2c35cda7078aa752f04c769d0ea36ad
drakenevadie19 <drakennievadje19@gmail.com>
2024-05-16 19:10:01 -0500

    New Change:  Added function for adding button, checked at MongoDB Database, also when successfully added new record, navigate to diary history page

--- front-end/src/app/add-diary/page.js
+++ front-end/src/app/add-diary/page.js
@@ -1,36 +1,90 @@
-'use client'
+"use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const initialFormData = {
-    title: '', 
-    description: ''
-}
+  title: "",
+  description: "",
+};
 
 export default function AddDiary() {
+  const [diaryFormData, setDiaryFormData] = useState(initialFormData);
 
-    const [diaryFormData, setDiaryFormData] = useState(initialFormData);
+  const router = useRouter();
 
-    console.log(diaryFormData);
+  async function handleAdding() {
+    const response = await fetch('/api/diary/add-new-diary', {
+        method: "POST", 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(diaryFormData)
+    });
+    
+    const result = await response.json();
+    console.log(result);
+    if (result?.success) {
+        setDiaryFormData(initialFormData);
 
-    return (
-        <>
-            <div className="flex min-h-screen flex-col p-8">
-                <h1 className="font-bold text-lg mb-3">Add a new Diary for healing your day</h1>
-                <div className="flex flex-col gap-4">
-                    <div className="flex flex-col gap-3">
-                        <label><b><i>Enter Diary title</i></b></label>
-                        <input name="title" placeholder="Enter your diary title" className="border border-red-500 p-3 outline-none" value={diaryFormData['title']} onChange={(e) => setDiaryFormData({...diaryFormData, title: e.target.value})} />
-                    </div>
-                    <div className="flex flex-col gap-3">
-                        <label><b><i>Enter Diary title</i></b></label>
-                        <textarea name="description" placeholder="Enter your thought here" rows={6} className="border border-red-500 p-3 outline-none" value={diaryFormData['description']} onChange={(e) => setDiaryFormData({...diaryFormData, description: e.target.value})} />
-                    </div>
-                    <div>
-                        <button className="border border-red-500 p-4 bg-black text-white" onClick={() => alert(`${diaryFormData['title']} ${diaryFormData['description']}`)}>Add</button>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+        // When adding success fully, navigate to diary-list page
+        router.push('/diary-list')
+    }
+  }
+
+  return (
+    <>
+      <div className="flex min-h-screen flex-col p-8">
+        <h1 className="font-bold text-lg mb-3">
+          Add a new Diary for healing your day
+        </h1>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-3">
+            <label>
+              <b>
+                <i>Enter Diary title</i>
+              </b>
+            </label>
+            <input
+              name="title"
+              placeholder="Enter your diary title"
+              className="border border-red-500 p-3 outline-none"
+              value={diaryFormData["title"]}
+              onChange={(e) =>
+                setDiaryFormData({ ...diaryFormData, title: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-3">
+            <label>
+              <b>
+                <i>Enter Diary title</i>
+              </b>
+            </label>
+            <textarea
+              name="description"
+              placeholder="Enter your thought here"
+              rows={6}
+              className="border border-red-500 p-3 outline-none"
+              value={diaryFormData["description"]}
+              onChange={(e) =>
+                setDiaryFormData({
+                  ...diaryFormData,
+                  description: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div>
+            <button
+              className="border border-red-500 p-4 bg-black text-white"
+              onClick={handleAdding}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
